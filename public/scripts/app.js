7fbf8b7877228a8f4d2ab00ca9b004b8a7ba199d
Jake <jakefantin@gmail.com>
2019-10-10 00:16:04 +0000

    added ajax to save new tweets. and read/display new tweets and saved tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,31 +5,6 @@
  */
 $(document).ready(() => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = (tweet) => {
     const markup = `
   <article>
@@ -54,17 +29,83 @@ $(document).ready(() => {
   </footer>
   </article>
   `;
-  return markup;
+    return markup;
   };
 
+  const createNewTweetElement = (tweet) => {
+    const markup = `
+  <article class="load">
+  <header>
+    <div class="tweeter-profile">
+      <img class="tweeter-pic" src='${tweet['user']['avatars']}'>
+      <a class="tweeter-name">${tweet['user']['name']}</a>
+    </div>
+    <a class="tweeter-tag">${tweet['user']['handle']}</a>
+  </header>
+  <div class="tweet-textbox">
+    <p class="tweet-text">${tweet['content']['text']}</p>
+  </div>
+  <footer>
+    <a class="tweet-date">${new Date(tweet['created_at'])}</a>
+    <div class="tweet-icons">
+        <i class="fa fa-flag"></i>
+        <i class="fa fa-heart"></i>
+        <i class="fa fa-refresh"></i>
+    </div>
+
+  </footer>
+  </article>
+  `;
+    return markup;
+  };
+
+
   const renderTweets = function(tweets) {
-    console.log('hey baby');
     const section = $('.old-tweets');
-    for(const tweet of tweets){
+    for (const tweet of tweets) {
       let article = createTweetElement(tweet);
-      section.append(article);
+      section.prepend(article);
     }
   }
+  // renderTweets(data);
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({ method: 'GET', url: '/tweets/' })
+      .then((res) => {
+        renderTweets(res);
+      })
+  }
+  loadTweets();
+
+  const loadNewTweet = function() {
+    $.ajax({ method: 'GET', url: '/tweets/' })
+      .then((res) => {
+        const section = $('.old-tweets');
+        let article = createTweetElement(res[res.length-1]);
+        section.prepend(article);
+        // const articleLoad = $('article .load');
+        // articleLoad.addClass('show');
+        // articleLoad.removeClass('load');
+      })
+  }
+
+  $('#tweet-zone').on('submit', function(event) {
+    event.preventDefault();
+    const tweet = $('#new-tweet');
+    // Form Validation
+    if (tweet[0].textLength === 0) {
+      alert("Needs text to twit.");
+    } else if (tweet[0].textLength > 140) {
+      alert("Too many characters.");
+    } else {
+      $.ajax({ method: 'POST', url: '/tweets/', data: $(this).serialize() })
+        .then(() => {
+          loadNewTweet()
+          tweet.val('');
+          let counter =  $('#letter-count');
+          counter.html(0);
+          tweet.attr('rows','1');
+        });
+    }
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   const counter = $('#letter-count');
   let inputRows = '2';
-  console.log('things have loaded');
   $('#new-tweet').on('input', function() {
     const currentLength = $(this).val().length;
     const maxChars = 140;
@@ -9,8 +8,6 @@ $(document).ready(function() {
     // const lineLength = Math.ceil(($(this).width())/parseInt($(this).css('font-size'),10))*2.03;
     // const lineLength = $(this);
     counter.html(maxChars-currentLength);
-    console.log($(this)[0].scrollHeight);
-    console.log($(this)[0].clientHeight);
     if($(this)[0].scrollHeight > $(this)[0].clientHeight){
       $(this).attr('rows', inputRows);
       inputRows++;
@@ -20,6 +17,9 @@ $(document).ready(function() {
       counter.addClass('red');
     } else {
       counter.removeClass('red');
+    }
+    if(currentLength === 0){
+      $(this).attr('rows', '1');
     }
   });
 });
