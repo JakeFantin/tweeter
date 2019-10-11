85e6a07f27a8c66a9d02679ba0702035033bc6c3
Jake <jakefantin@gmail.com>
2019-10-11 01:22:39 +0000

    originally had a hack to make the header item fixed on screen inside of flexbox, changed it to make the other element scroll in place

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -39,34 +39,6 @@ $(document).ready(() => {
     return markup;
   };
 
-  const createNewTweetElement = (tweet) => {
-    const markup = `
-  <article class="load">
-  <header>
-    <div class="tweeter-profile">
-      <img class="tweeter-pic" src='${tweet['user']['avatars']}'>
-      <a class="tweeter-name">${escape(tweet['user']['name'])}</a>
-    </div>
-    <a class="tweeter-tag">${escape(tweet['user']['handle'])}</a>
-  </header>
-  <div class="tweet-textbox">
-    <p class="tweet-text">${escape(tweet['content']['text'])}</p>
-  </div>
-  <footer>
-    <a class="tweet-date">${new Date(tweet['created_at'])}</a>
-    <div class="tweet-icons">
-        <i class="fa fa-flag"></i>
-        <i class="fa fa-heart"></i>
-        <i class="fa fa-refresh"></i>
-    </div>
-
-  </footer>
-  </article>
-  `;
-    return markup;
-  };
-
-
   const renderTweets = function(tweets) {
     const section = $('.old-tweets');
     for (const tweet of tweets) {
@@ -74,7 +46,6 @@ $(document).ready(() => {
       section.prepend(article);
     }
   }
-  // renderTweets(data);
 
   const loadTweets = function() {
     $.ajax({ method: 'GET', url: '/tweets/' })
